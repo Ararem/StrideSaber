b90ff678b64e7948c604996b05ec2a6fb8e1e246
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2021-08-24 11:10:20 +1000

    Start doing actual generation

--- StrideSaber/Testing/StaticGenTesting.cs
+++ StrideSaber/Testing/StaticGenTesting.cs
@@ -4,34 +4,9 @@ using StrideSaber.SourceGenerators.StaticInstanceGeneration;
 
 namespace StrideSaber.Testing
 {
-	//Should be fine
 	[GenerateStaticInstanceMembers]
-	public class Class
+	public class StaticInstGenClass
 	{
-		
-	}
-	[GenerateStaticInstanceMembers]
-	public struct Struct
-	{
-		
-	}
-	
-	[GenerateStaticInstanceMembers]
-	public record Record
-	{
-		
-	}
-	
-	[GenerateStaticInstanceMembers]
-	public class Error_TooMany
-	{
-		[TargetInstanceMember] public Error_TooMany Member1;
-		[TargetInstanceMember] public Error_TooMany Member2;
-	}
 
-	[GenerateStaticInstanceMembers]
-	public static class Error_Static
-	{
-		
 	}
 }

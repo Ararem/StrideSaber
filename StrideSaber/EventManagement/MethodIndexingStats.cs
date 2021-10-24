62bdb48dadfaf892d8e4fe920cfbacd785a0bf7f
EternalClickbail <rowanradosavmcrae@gmail.com>
2021-10-24 16:24:12 +1000

    Convert Indexing Stats to properties

--- StrideSaber/EventManagement/MethodIndexingStats.cs
+++ StrideSaber/EventManagement/MethodIndexingStats.cs
@@ -14,27 +14,31 @@ namespace StrideSaber.EventManagement
 		/// <summary>
 		/// The number of <see cref="Assembly">Assemblies</see> that were found and scanned
 		/// </summary>
-		public uint AssemblyScanCount;
+		public uint AssemblyScanCount { get; set; }
+
 		/// <summary>
 		/// The number of <see cref="Type">Types</see> that were found and scanned
 		/// </summary>
-		public uint TypeScanCount;
+		public uint TypeScanCount { get; set; }
+
 		/// <summary>
 		/// The number of <see cref="MethodInfo">Methods</see> that were found and scanned. This includes methods that were invalid or were marked for several event types
 		/// </summary>
-		public uint MethodScanCount;
+		public uint MethodScanCount { get; set; }
 
 		/// <summary>
 		/// How many methods were found that were invalid (e.g. instance, too many parameters, generic, etc). If a method declaration is valid (e.g. static, void, no params), but has one valid event target and one invalid target, this will be incremented once per each invalid target
 		/// </summary>
-		public uint InvalidMethodCount;
+		public uint InvalidMethodCount { get; set; }
+
 		/// <summary>
 		/// How many methods were found, counting each method once, even if it was marked for multiple event types (e.g. 3 methods marked twice = 3)
 		/// </summary>
-		public uint ActualMethodCount;
+		public uint ActualMethodCount { get; set; }
+
 		/// <summary>
 		/// How many methods were found, counting once per valid event type (e.g. 3 methods marked twice = 6)
 		/// </summary>
-		public uint DuplicateMethodCount;
+		public uint DuplicateMethodCount { get; set; }
 	}
 }

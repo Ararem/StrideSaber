c8bda96ed203326baa3b88093314dbff6e2bf700
EternalClickbail <rowanradosavmcrae@gmail.com>
2021-10-24 16:30:45 +1000

    Get rid of testing code
    I did back it up though

--- StrideSaber/EventManagement/EventManager.cs
+++ StrideSaber/EventManagement/EventManager.cs
@@ -2,6 +2,7 @@
 using Serilog;
 using Serilog.Context;
 using Stride.Core.Extensions;
+using StrideSaber.EventManagement.Events;
 using StrideSaber.Modding;
 using System;
 using System.Collections.Concurrent;
@@ -186,9 +187,9 @@ namespace StrideSaber.EventManagement
 				{
 					//It's an object returning method, wrap it into a Returns_Param_EventWrapper<TEvent>
 					Type wrapperType = typeof(Returns_Param_EventWrapper<>)
-							.MakeGenericType(paramType);                                                             //Have to pass in the generic type arg
+							.MakeGenericType(paramType);                                                              //Have to pass in the generic type arg
 					Delegate del = method.CreateDelegate(typeof(Func<,>).MakeGenericType(paramType, typeof(object))); //Generalise the func delegate type. We can safely cast the return 'down' to object, but we mustn't do so for the event type
-					wrapper = (EventWrapper)Activator.CreateInstance(wrapperType, del)!;                             //The constructor should have an appropriate input type (I hope)
+					wrapper = (EventWrapper)Activator.CreateInstance(wrapperType, del)!;                              //The constructor should have an appropriate input type (I hope)
 				}
 			}
 
@@ -244,3 +245,81 @@ namespace StrideSaber.EventManagement
 	#endregion
 	}
 }
+
+#region Testing
+
+//Here's a test class I used to check that all the code worked properly when I rewrote the class
+// using StrideSaber.EventManagement;
+// using StrideSaber.EventManagement.Events;
+//
+// //ReSharper disable all
+// namespace StrideSaber.Hacks
+// {
+// 	/// <summary>
+// 	///  A test class
+// 	/// </summary>
+// 	public sealed class TestClass
+// 	{
+// 		[EventMethod(typeof(TestEvent))]
+// 		#pragma warning disable CA1822
+// 		private void Fail_NonStatic()
+// 		{
+// 		}
+// 		#pragma warning restore CA1822
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Fail_TooManyParams(object o1, object o2, object o3)
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Fail_NotInheritedFromEvent(object o1)
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Success_VoidNoParams()
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static object? Success_ObjectReturnNoParams()
+// 		{
+// 			return null;
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Fail_CannotCastAttributeToParam(GameLoadEvent e)
+// 		{
+// 		}
+// 		
+// 		[EventMethod(typeof(Event))]
+// 		private static void Fail_CannotCastReverse(GameLoadEvent e)
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Success_Void_BaseEventParam(Event e)
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static void Success_Void_InheritedEventParam(TestEvent e)
+// 		{
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static object? Success_ReturnObject_BaseEventParam(Event e)
+// 		{
+// 			return null;
+// 		}
+//
+// 		[EventMethod(typeof(TestEvent))]
+// 		private static string? Success_ReturnObject_InheritedEventParam(TestEvent e)
+// 		{
+// 			return null;
+// 		}
+// 	}
+// }
+
+#endregion

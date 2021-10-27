39a05e88b4215fac185bdf54565741b8afb47e46
EternalClickbail <rowanradosavmcrae@gmail.com>
2021-10-27 11:11:48 +1000

    Fix UI colours

---
+++ StrideSaber/Hacks/NoteComponent.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Stride.Engine;
+
+namespace StrideSaber.Hacks
+{
+	/// <summary>
+	/// A component that can be used to attach a note to an entity
+	/// </summary>
+	[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+	public sealed class NoteComponent : ScriptComponent
+	{
+		/// <summary>
+		/// The note for the entity
+		/// </summary>
+		public string? Note = null;
+	}
+}

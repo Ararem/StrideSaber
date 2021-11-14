02ad961c98a5240b0630361d86914c78a38cb3d4
EternalClickbail <rowanradosavmcrae@gmail.com>
2021-11-14 10:04:29 +1000

    Rewrite ProgressUiController to reuse and cache everything
    No i haven't run it yuet

--- StrideSaber/SceneSpecific/Progress Ui/ProgressUiController.cs
+++ StrideSaber/SceneSpecific/Progress Ui/ProgressUiController.cs
@@ -1,5 +1,6 @@
 ﻿using JetBrains.Annotations;
 using LibEternal.ObjectPools;
+using Stride.Core.Extensions;
 using Stride.Core.Mathematics;
 using Stride.Core.Serialization;
 using Stride.Engine;
@@ -14,7 +15,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using SLog = Serilog.Log;
 
@@ -36,7 +36,7 @@ namespace StrideSaber.SceneSpecific.Progress_Ui
 		/// </summary>
 		public UrlReference<UILibrary> LibraryReference = null!;
 
-		private UILibrary library = null!;
+		private UILibrary  library         = null!;
 		private StackPanel indicatorsPanel = null!;
 
 	#region Testing
@@ -45,7 +45,7 @@ namespace StrideSaber.SceneSpecific.Progress_Ui
 		public override void Start()
 		{
 			indicatorsPanel = Ui.Page.RootElement.FindVisualChildOfType<StackPanel>("Indicators");
-			library = Content.Load(LibraryReference);
+			library         = Content.Load(LibraryReference);
 			//Create some tasks for fun
 			Task.Run(AsyncTaskCreator);
 			SLog.Information("Task creator started");
@@ -85,7 +85,7 @@ namespace StrideSaber.SceneSpecific.Progress_Ui
 		private static async Task AsyncTaskTest(Action<float> updateProgress)
 		{
 			DateTime start = DateTime.Now;
-			DateTime end = start + TimeSpan.FromMilliseconds(r.Next(0, 5000));
+			DateTime end   = start + TimeSpan.FromMilliseconds(r.Next(0, 5000));
 			while (DateTime.Now < end)
 			{
 				await Task.Delay(50);
@@ -99,131 +99,146 @@ namespace StrideSaber.SceneSpecific.Progress_Ui
 
 		private static Color
 				backgroundColour = new(69, 69, 69, 255),
-				tickColour = new(211, 211, 211, 255);
+				tickColour       = new(211, 211, 211, 255);
+
+		/// <summary>
+		/// The tasks that are currently being shown right now
+		/// </summary>
+		private readonly Queue<TaskDisplay> shownTaskDisplays = new();
+
+		private readonly Queue<TaskDisplay> cachedTaskDisplays = new();
 
 		/// <summary>
 		/// Updates the UI from the current tasks and their statuses
 		/// </summary>
-		// PERF: Hot path, needs lots of performance improvements, namely:
-		// 1: Task string formatting handling
-		// 2: Child finding for components - Uses LINQ
-		// 3: Almost nothing is cached, this would be a massive improvement
-		// 4: Colours for sliders - perhaps internally create a list of ~1000 combinations and pull out the closest approximation?
-		// Texture reuse - `tex.SetData(Game.GraphicsContext.CommandList, new[]{colour});` Needs texture to have `GraphicsResourceUsage.Dynamic`
 		public override void Update()
 		{
 			//Get all the current tasks, ordered by ID
 			var tasks = TrackedTask.CloneAllInstances()
-			                       .OrderBy(static t => t.Id)
-			                       .ToArray();
+									.OrderBy(static t => t.Id)
+									.ToArray();
 
-			StringBuilder sb = StringBuilderPool.GetPooled();
+			//First off, move all the current displays into the cache
+			while (shownTaskDisplays.TryDequeue(out var display))
+			{
+				//Remove it from the visual tree so we don't see it
+				indicatorsPanel.Children.SwapRemove(display.IndicatorRootElement);
+				cachedTaskDisplays.Enqueue(display);
+			}
+
 			for (int i = 0; i < tasks.Length; i++)
 			{
-				UIElement indicator;
 				TrackedTask task = tasks[i];
-				//Reuse an old ui element if we have one
-				if (indicatorsPanel.Children.Count > i)
-				{
-					indicator = indicatorsPanel.Children[i];
-				}
-				//Need to create a new element
-				else
-				{
-					indicator = library.InstantiateElement<UIElement>("Progress Indicator");
-					indicatorsPanel.Children.Add(indicator);
-				}
+				//Try pull out an existing display, else create a new one
+				#pragma warning disable 8600
+				if (!cachedTaskDisplays.TryDequeue(out TaskDisplay display))
+					display = new TaskDisplay(this);
+				#pragma warning restore 8600
 
+
+				//Update the display from the current task
+				display.Update(task, Game.GraphicsContext);
+
+				//We have an updated task display element, add it to the panel so that it gets drawn
+				indicatorsPanel.Children.Add(display.IndicatorRootElement);
+			}
+		}
+
+		private class TaskDisplay
+		{
+			/// <summary>
+			/// Cached reference to the slider child element
+			/// </summary>
+			private readonly Slider slider;
+
+			/// <summary>
+			/// The <see cref="Texture">Texture</see> for the <see cref="slider"/> section
+			/// </summary>
+			private readonly Texture background, foreground, tick;
+
+			/// <summary>
+			/// Cached reference to the text block child.
+			/// </summary>
+			private readonly TextBlock textBlock;
+
+			/// <summary>
+			/// The root <see cref="UIElement"/> for this task display. Should be a child of the indicators panel when being displayed (not cached)
+			/// </summary>
+			internal readonly Border IndicatorRootElement;
+
+			internal TaskDisplay(ProgressUiController controller)
+			{
+				//These two I know won't be null, but bloody compiler thinks they will :(
+				slider               = null!;
+				textBlock            = null!;
+				IndicatorRootElement = controller.library.InstantiateElement<Border>("Progress Indicator");
+
+				//Loop over the children to try and find the slider and TextBlock child elements
+				foreach (UIElement e in IndicatorRootElement.VisualChildren)
+					switch (e)
+					{
+						case Slider s:
+							slider = s;
+							break;
+						case TextBlock t:
+							textBlock = t;
+							break;
+					}
+
+				//Create and assign our textures for the slider
+				foreground = NewTex(controller.GraphicsDevice);
+				tick       = NewTex(controller.GraphicsDevice);
+				background = NewTex(controller.GraphicsDevice);
+
+				slider.TrackForegroundImage = new SpriteFromTexture { Texture = foreground };
+				slider.TrackBackgroundImage = new SpriteFromTexture { Texture = background };
+				slider.TickImage            = new SpriteFromTexture { Texture = tick };
+
+				static Texture NewTex(GraphicsDevice graphicsDevice)
+				{
+					//R8G8B8A8_UNorm_SRgb seems to work without fucking stuff up
+					//Because Stride.Core.Mathematics.Color is 1 byte per channel, RGBA
+					return Texture.New2D(
+							graphicsDevice,
+							1,
+							1,
+							PixelFormat.R8G8B8A8_UNorm_SRgb,
+							new[] { Color.Purple }, //Purple is the default so tha twe can easily tell if we forgot to update it
+							usage: GraphicsResourceUsage.Dynamic
+					);
+				}
+			}
+
+			// PERF: Hot path, needs lots of performance improvements, namely:
+			// 1: Task string formatting handling
+			// 2: Child finding for components - Uses LINQ
+			// 3: Almost nothing is cached, this would be a massive improvement
+			// 4: Colours for sliders - perhaps internally create a list of ~1000 combinations and pull out the closest approximation?
+			// Texture reuse - `tex.SetData(Game.GraphicsContext.CommandList, new[]{colour});` Needs texture to have `GraphicsResourceUsage.Dynamic`
+			internal void Update(TrackedTask task, GraphicsContext graphicsContext)
+			{
 				//Set the text and slider values for the task
-				sb.Clear();
-				//The Builder is ~50% slower than interpolation, but ~40% less memory
-				sb.Append(task.Name)
-				  .Append(":\t")
-				  .Append(task.Progress.ToString("p0").PadLeft(3));
-				indicator.FindVisualChildOfType<TextBlock>().Text = sb.ToString();
-				Slider slider = indicator.FindVisualChildOfType<Slider>();
+				//Using a StringBuilder is ~50% slower than interpolation, but ~40% less memory
+				textBlock.Text = StringBuilderPool.BorrowInline(static (sb, task) =>
+								sb.Append(task.Name)
+								.Append(":\t")
+								.Append(task.Progress.ToString("p0").PadLeft(3))
+						, task);
 				slider.Value = task.Progress;
 
 				//Try and give the task a nice little colour too
 				//This one is based on the hash of the task ID
 				Color uniqueTaskColour = Color.FromRgba(task.Id.GetHashCode());
-				uniqueTaskColour.A = 255;
-				//This one is based on the progression of the task
-				//TODO: This is decent for now, but maybe fix the tick lines
-				//Assign all the colours
-				(indicator as Border)!.BorderColor = uniqueTaskColour; //Unique border for each task
-				//Background and tick sprites should be consistent so store them, but unique sprites won't be because they will keep changing
-				//FIXME: Need to dispose of the old value here, this is a memory leak
-				slider.TrackBackgroundImage = SpriteFromColour(backgroundColour);
-				slider.TickImage = SpriteFromColour(tickColour);
-				slider.TrackForegroundImage = SpriteFromColour(uniqueTaskColour);
+				uniqueTaskColour.A = 255; //Ensure we can see the task so set the alpha to full
+				//TODO: This is decent for now, but maybe fix the tick lines (contrasting to fg and bg)
+				//FIXME: Do we meed to dispose of the old value here? Was a memory leak but i did change this a lot
+				IndicatorRootElement.BorderColor = uniqueTaskColour;
+				//Thankfully we have the textures cached so we don't need to re-fetch them
+				//And now since they're 1x1 textures we can simply overwrite their colours
+				background.SetData(graphicsContext.CommandList, new[] { backgroundColour });
+				foreground.SetData(graphicsContext.CommandList, new[] { uniqueTaskColour });
+				tick.SetData(graphicsContext.CommandList, new[] { tickColour });
 			}
-
-			StringBuilderPool.ReturnPooled(sb);
-
-			//Remove any extra elements we didn't use
-			//So we start from our task count, and go up to our child element count
-			//Task = 10
-			//Children = 20
-			//Start at index 10 in children (10-1=9 is last task)
-			//End at children -1
-			//Do this in reverse so we can use `RemoveAt()` (which is faster than `Remove()`) without any complex maths
-			for (int i = indicatorsPanel.Children.Count - 1; i >= tasks.Length; i--) indicatorsPanel.Children.RemoveAt(i);
-		}
-
-		/// <summary>
-		/// A dictionary cache that maps a <see cref="Color"/> to a pre-generated sprite
-		/// </summary>
-		private static readonly Dictionary<Color, ISpriteProvider> CachedSpriteFromColour = new();
-
-		internal ISpriteProvider SpriteFromColour(Color colour)
-		{
-			//Rounds `x` to the nearest `n`
-			//Aka Round(14,5) => 15
-			static byte Round(byte x, byte n)
-			{
-				//This rounds it to the closest value of `n`, but it might be out of range
-				float res = (MathF.Round((float)x / n, MidpointRounding.AwayFromZero) * n);
-				//if it's too large, bump it down
-				if (res > byte.MaxValue) res -= n;
-				return (byte)res;
-			}
-
-			/*
-			 * 'Round' the colour so that we don't have to check for as many and store as many colours
-			 * Essentially reducing the bit depth here
-			 * channelUniqueColours is how many unique colours per channel
-			 * So if channelUniqueColours == 32, each channel (R,G,B) has 32 unique levels it can be
-			 * rounding = 256/32 = 8, so each channel is rounded to the nearest 8
-			 * So the total colour count is 32^3=32,768, which is 512 times fewer than full 8-bit per channel
-			 * So very good for our memory usage
-			 */
-			const byte channelUniqueColours = 16;
-			const byte rounding = byte.MaxValue / channelUniqueColours;
-			colour.R = Round(colour.R, rounding);
-			colour.G = Round(colour.G, rounding);
-			colour.B = Round(colour.B, rounding);
-			colour.A = Round(colour.A, rounding);
-			//Try get it from the cache first, as this is the most performant
-			if (CachedSpriteFromColour.ContainsKey(colour))
-				return CachedSpriteFromColour[colour];
-
-			SpriteFromTexture sprite = new()
-			{
-					Texture =
-							//R8G8B8A8_UNorm_SRgb seems to work without fucking stuff up
-							//Because Stride.Core.Mathematics.Color is 1 byte per channel, RGBA
-							Texture.New2D(
-									GraphicsDevice,
-									1,
-									1,
-									PixelFormat.R8G8B8A8_UNorm_SRgb,
-									new[] { colour }
-							)
-			};
-			CachedSpriteFromColour[colour] = sprite;
-
-			return sprite;
 		}
 	}
 }
